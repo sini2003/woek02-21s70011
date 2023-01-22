bdea73b0596cb6135885e80107319dcd691714e9
sini2003 <kobataku.0201@gmail.com>
2023-01-23 00:06:17 +0900

    s

---
+++ work2/Assets/Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 posX = transform.position;
+        posX.x += -0.01f;
+        transform.position = posX;
+    }
+}
